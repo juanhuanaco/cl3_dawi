de715b08c836059a338fb4e3b5879523b171cf0b
Juan Huanaco <juanhuanacodev@gmail.com>
2023-06-22 12:18:25 -0500

    base files

---
+++ src/main/java/com/cibertec/Cl3DawiHuanacoquispejuandiegoApplication.java
@@ -0,0 +1,13 @@
+package com.cibertec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cl3DawiHuanacoquispejuandiegoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cl3DawiHuanacoquispejuandiegoApplication.class, args);
+	}
+
+}
